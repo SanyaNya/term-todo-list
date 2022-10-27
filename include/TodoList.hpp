d60708b495a2a319ffd3972b4155453d4418b37f
SanyaNya <sanyanya4ever@gmail.com>
2022-10-27 12:58:32 +0600

    TodoList struct impl

---
+++ include/TodoList.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <unordered_map>
+#include "Task.hpp"
+
+namespace todolist
+{
+
+class TodoList
+{
+    std::unordered_map<std::string, Task> tasks;
+
+public:
+    void add(Task&& task);
+    void done(const std::string& name);
+    void update(const std::string& name, Task&& task);
+    void del(const std::string& name);
+
+    auto begin() noexcept;
+    auto begin() const noexcept;
+    auto cbegin() const noexcept;
+
+    auto end() noexcept;
+    auto end() const noexcept;
+    auto cend() const noexcept;
+};
+
+} //namespace todolist
---
+++ src/TodoList.cpp
@@ -0,0 +1,34 @@
+#include "TodoList.hpp"
+
+namespace todolist
+{
+
+void TodoList::add(Task&& task)
+{
+    tasks.emplace(task.name, std::move(task));
+}
+
+void TodoList::done(const std::string& name)
+{
+    tasks.at(name).status = true;
+}
+
+void TodoList::update(const std::string& name, Task&& task)
+{
+    tasks.at(name) = std::move(task);
+}
+
+void TodoList::del(const std::string& name)
+{
+    tasks.erase(name);
+}
+
+auto TodoList::begin() noexcept { return tasks.begin(); }
+auto TodoList::begin() const noexcept { return tasks.begin(); }
+auto TodoList::cbegin() const noexcept { return tasks.cbegin(); }
+
+auto TodoList::end() noexcept { return tasks.end(); }
+auto TodoList::end() const noexcept { return tasks.end(); }
+auto TodoList::cend() const noexcept { return tasks.cend(); }
+
+} //namespace todolist
