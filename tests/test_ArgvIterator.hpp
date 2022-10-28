727e35590c9157e094ae2f98f43b761d1498d351
SanyaNya <sanyanya4ever@gmail.com>
2022-10-28 16:36:05 +0600

    Tests for ArgvIterator

--- tests/test_ArgvIterator.hpp
+++ tests/test_ArgvIterator.hpp
@@ -9,6 +9,47 @@ TEST("ArgvIterator",
         ArgvIterator it(argv, argc);
         ArgvIterator end(argv, argc, null_sentinel{});
 
-        assert(*it == null_sentinel{});
-        
-    }))
+        assert(it == end);
+    }),
+    SUBTEST("One arg", []()
+    {
+        const char* argv[]{ "word=123" };
+        int argc = 1;
+
+        using namespace todolist::utils;
+
+        ArgvIterator it(argv, argc);
+        ArgvIterator end(argv, argc, null_sentinel{});
+
+        assert(it.token() == "word=123");
+        assert(std::equal(it, end, argv[0]));
+    }),
+    SUBTEST("Many args", []()
+    {
+        const char* argv[]
+            { "a", "abcd", "321.12", "qwerty=\"123 321\"" };
+
+        int argc = sizeof(argv)/sizeof(*argv);
+
+        std::string s;
+        for(const char* arg : argv)
+        {
+            s += arg;
+            s += ' ';
+        }
+        s.pop_back();
+
+        using namespace todolist::utils;
+
+        ArgvIterator it(argv, argc);
+        ArgvIterator end(argv, argc, null_sentinel{});
+
+        assert(std::equal(it, end, s.begin()));
+
+        for(const char* arg : argv)
+        {
+            assert(it.token() == arg);
+            it.to_next_token();
+        }
+    })
+)
