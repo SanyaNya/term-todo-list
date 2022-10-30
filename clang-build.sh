d2eb8ea050fa02926e28c16c3bf7e02ec443caff
SanyaNya <sanyanya4ever@gmail.com>
2022-10-30 20:21:18 +0600

    Normal debug/release builds on gcc/clang

--- clang-build.sh
+++ clang-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -DBUILD_TESTS=1 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S . -B build
+cmake -DBUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S . -B build
 cmake --build build
--- gcc-build.sh
+++ gcc-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -DBUILD_TESTS=1 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S . -B build
+cmake -DBUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S . -B build
 cmake --build build
