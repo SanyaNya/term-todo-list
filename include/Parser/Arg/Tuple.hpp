d2c3e22cbf33032b1e91c19258e464ab55d6435e
SanyaNya <sanyanya4ever@gmail.com>
2022-10-31 13:13:38 +0600

    Custom arg Tuple

---
+++ include/Parser/Arg/Tuple.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <tuple>
+#include "Parser/parse.hpp"
+
+namespace todolist::Parser
+{
+
+namespace detail
+{
+
+template<typename It, typename Tuple, size_t I = 0>
+inline bool tuple_parse(It& begin, It end, Tuple& tuple) noexcept
+{
+    using T = std::tuple_element_t<I, Tuple>;
+
+    if(begin == end) return false;
+
+    if(auto opt = Parser::parse_arg<T>(begin, end))
+    {
+        std::get<I>(tuple) = opt.value();
+        return true;
+    }
+    else
+        return false;
+
+    if constexpr(I+1 != std::tuple_size_v<Tuple>)
+        return tuple_parse(begin, end, tuple);
+}
+
+} //namespace detail
+
+template<typename ... Ts>
+struct Tuple
+{
+    static constexpr std::string_view token_name = "Arg";
+
+    std::tuple<Ts...> value;
+
+    template<typename It>
+    static std::optional<Tuple> parse(It& begin, It end) noexcept
+    {
+        Tuple t;
+        It initial_begin = begin;
+
+        if(!detail::tuple_parse(begin, end, t.value))
+        {
+            begin = initial_begin;
+            return std::nullopt;
+        }
+
+        return t;
+    }
+};
+
+} //namespace todolist::Parser
