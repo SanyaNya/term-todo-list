5f7b93132422484fd1a16175dbdbd19d87ab2c7e
SanyaNya <sanyanya4ever@gmail.com>
2022-10-31 20:17:31 +0600

    Task serialization

--- include/Task.hpp
+++ include/Task.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <ctime>
+#include "utils/serialization.hpp"
 
 namespace todolist
 {
@@ -13,6 +14,27 @@ struct Task
     std::time_t date;
     std::string category;
     bool status;
+
+    void serialize(std::ostream& os) const
+    {
+        utils::serialize(os, name);
+        utils::serialize(os, description);
+        utils::serialize(os, date);
+        utils::serialize(os, category);
+        utils::serialize(os, status);
+    }
+
+    static Task deserialize(std::istream& is)
+    {
+        return Task
+        {
+            utils::deserialize<std::string>(is),
+            utils::deserialize<std::string>(is),
+            utils::deserialize<std::time_t>(is),
+            utils::deserialize<std::string>(is),
+            utils::deserialize<bool>(is)
+        };
+    }
 };
 
 } //namespace todolist
