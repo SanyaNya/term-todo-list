c5818de1b35e3fa488f1143a556de340c9ab0709
SanyaNya <sanyanya4ever@gmail.com>
2022-11-01 18:57:28 +0600

    Include all traits in one file

---
+++ include/traits/traits.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "has_begin_end.hpp"
+#include "has_data.hpp"
+#include "has_size.hpp"
+#include "has_value_type.hpp"
+#include "is_deserializable.hpp"
+#include "is_matchable.hpp"
+#include "is_named.hpp"
+#include "is_parsable.hpp"
+#include "is_serializable.hpp"
+#include "is_variant.hpp"
