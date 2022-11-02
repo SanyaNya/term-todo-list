f1fb62cf5e3dfb9094735c771ebc23657af1dbd2
SanyaNya <sanyanya4ever@gmail.com>
2022-11-02 18:31:21 +0600

    Rework serialization again :)

--- include/serialization/deserialize.hpp
+++ include/serialization/deserialize.hpp
@@ -10,64 +10,98 @@ namespace todolist::serialization
 {
 
 template<typename T>
-inline
-std::enable_if_t<traits::is_deserializable_v<T>, T> f(std::istream& is)
+inline std::decay_t<T> deserialize(std::istream& is);
+
+namespace detail
 {
-    return T::deserialize(is);
+
+template<typename T, size_t ... Is>
+inline T expand_deserialize(std::istream& is, std::index_sequence<Is...>)
+{
+    return T{deserialize<std::tuple_element_t<Is, T>>(is)...};
 }
 
 template<typename T>
-inline 
-std::enable_if_t<
-    !traits::is_deserializable_v<T> &&
-    !std::is_integral_v<T>          &&
-    std::is_trivially_copyable_v<T> &&
-    std::is_default_constructible_v<T>, T> deserialize(std::istream& is)
+inline T deserialize_h(std::istream& is)
 {
-    T t;
-    is.read(reinterpret_cast<char*>(&t), sizeof(T));
+    if constexpr(traits::is_deserializable_v<T>)
+        return T::deserialize(is);
+    else
+    {
+        if constexpr(
+            std::is_trivially_copyable_v<T> && 
+            std::is_default_constructible_v<T>)
+        {
+            static_assert(
+                !std::is_integral_v<T>, 
+                "Serialization of integrals is forbidden, use net_uint types");
 
-    return t;
+            T t;
+            is.read(reinterpret_cast<char*>(&t), sizeof(T));
+
+            return t;
+        }
+        else
+        {
+            if constexpr(
+                traits::has_begin_end_v<T> && traits::has_size_v<T>)
+            {
+                if constexpr(
+                    traits::has_data_v<T> &&
+                    traits::has_value_type_v<
+                        T, std::is_trivially_copyable> &&
+                    traits::is_resizable_v<T>)
+                {
+                    size_t sz{
+                        static_cast<std::uint64_t>(
+                            deserialize<net_uint64>(is))};
+                    T t;
+                    t.resize(sz);
+
+                    is.read(
+                        std::data(t), 
+                        static_cast<std::streamsize>(
+                            sz * sizeof(typename T::value_type)));
+
+                    return t;
+                }
+                else if constexpr(
+                    traits::has_value_type_v<T> &&
+                    traits::is_range_constructible_v<T>)
+                {
+                    const auto f = [&is]()
+                    { 
+                        return deserialize<typename T::value_type>(is); 
+                    };
+
+                    size_t sz{
+                        static_cast<std::uint64_t>(
+                            deserialize<net_uint64>(is))};
+
+                    return T
+                    {
+                        utils::func_iterator(f, 0), 
+                        utils::func_iterator(f, sz)
+                    };
+                }
+            }
+            else if constexpr(traits::is_tuple_like_v<T>)
+            {
+                return expand_deserialize<T>(
+                        is,
+                        std::make_index_sequence<
+                            std::tuple_size_v<T>>{});
+            }
+        }
+    }
 }
 
-template<typename T>
-inline 
-std::enable_if_t<
-    !traits::is_deserializable_v<T>                          &&
-    !std::is_trivially_copyable_v<T>                         &&
-    traits::has_value_type_v<T>                              &&
-    !traits::has_value_type_v<T, std::is_trivially_copyable> &&
-    traits::is_range_constructible_v<T>, T> deserialize(std::istream& is)
-{
-    const auto f = [&is]()
-    { 
-        return deserialize<typename T::value_type>(is); 
-    };
-
-    size_t sz{static_cast<std::uint64_t>(deserialize<net_uint64>(is))};
-    return T{utils::func_iterator(f, 0), utils::func_iterator(f, sz)};
-}
+} //namespace detail
 
 template<typename T>
-inline 
-std::enable_if_t<
-    !traits::is_deserializable_v<T>                         &&
-    !std::is_trivially_copyable_v<T>                        &&
-    std::is_default_constructible_v<T>                      &&
-    traits::is_resizable_v<T>                               &&
-    traits::has_value_type_v<T>                             &&
-    traits::has_value_type_v<T, std::is_trivially_copyable> &&
-    traits::has_data_v<T>, T> deserialize(std::istream& is)
+inline std::decay_t<T> deserialize(std::istream& is)
 {
-    size_t sz{static_cast<std::uint64_t>(deserialize<net_uint64>(is))};
-    T t;
-    t.resize(sz);
-
-    is.read(
-        std::data(t), 
-        static_cast<std::streamsize>(sz * sizeof(typename T::value_type)));
-
-    return t;
+    return detail::deserialize_h<std::decay_t<T>>(is);
 }
 
 } //namespace todolist::serialization
--- include/serialization/serialize.hpp
+++ include/serialization/serialize.hpp
@@ -2,61 +2,73 @@
 
 #include <iostream>
 #include "traits/traits.hpp"
+#include "net_integral.hpp"
 
 namespace todolist::serialization
 {
 
 template<typename T>
-inline std::enable_if_t<traits::is_serializable_v<T>> 
-serialize(std::ostream& os, const T& t)
+inline void serialize(std::ostream& os, const T& t);
+
+namespace detail
 {
-    t.serialize(os);
+
+template<typename T, size_t ... Is>
+void expand_serialize(
+        std::ostream& os, const T& t, std::index_sequence<Is...>)
+{
+    (serialize(os, std::get<Is>(t)), ...);
 }
 
-template<typename T>
-inline
-std::enable_if_t<
-    !traits::is_serializable_v<T> &&
-    !std::is_integral_v<T>        &&
-    std::is_trivially_copyable_v<T>>
-serialize(std::ostream& os, const T& t)
-{
-    os.write(reinterpret_cast<const char*>(&t), sizeof(T));
-}
+} //namespace detail
 
 template<typename T>
-inline
-std::enable_if_t<
-    !traits::is_serializable_v<T>                            &&
-    !std::is_trivially_copyable_v<T>                         &&
-    !traits::has_value_type_v<T, std::is_trivially_copyable> &&
-    traits::has_value_type_v<T, traits::is_serializable>     &&
-    traits::has_size_v<T>                                    &&
-    traits::has_begin_end_v<T>>
-serialize(std::ostream& os, const T& t)
+inline void serialize(std::ostream& os, const T& t)
 {
-    serialize(os, net_uint64(std::size(t)));
-    for(const auto& obj : t)
-        serialize(os, obj);
-}
+    if constexpr(traits::is_serializable_v<T>)
+        t.serialize(os);
+    else
+    {
+        if constexpr(std::is_trivially_copyable_v<T>)
+        {
+            static_assert(
+                !std::is_integral_v<T>, 
+                "Serialization of integrals is forbidden, use net_uint types");
 
-template<typename T>
-inline
-std::enable_if_t<
-    !traits::is_serializable_v<T>                           &&
-    !std::is_trivially_copyable_v<T>                        &&
-    traits::has_value_type_v<T>                             &&
-    traits::has_value_type_v<T, std::is_trivially_copyable> &&
-    !traits::has_value_type_v<T, traits::is_serializable>   &&
-    traits::has_size_v<T>                                   &&
-    traits::has_data_v<T>>
-serialize(std::ostream& os, const T& t)
-{
-    serialize(os, net_uint64(std::size(t)));
-    os.write(
-        std::data(t), 
-        static_cast<std::streamsize>(
-            std::size(t)*sizeof(typename T::value_type)));
+            os.write(reinterpret_cast<const char*>(&t), sizeof(T));
+        }
+        else
+        {
+            if constexpr(
+                traits::has_begin_end_v<T> && traits::has_size_v<T>)
+            {
+                if constexpr(
+                    traits::has_data_v<T> && 
+                    traits::has_value_type_v<
+                        T, std::is_trivially_copyable>)
+                {
+                    serialize(os, net_uint64(std::size(t)));
+                    os.write(
+                        std::data(t), 
+                        static_cast<std::streamsize>(
+                            std::size(t)*sizeof(typename T::value_type)));
+                }
+                else
+                {
+                    serialize(os, net_uint64(std::size(t)));
+                    for(const auto& obj : t)
+                        serialize(os, obj);
+                }
+            }
+            else if constexpr(traits::is_tuple_like_v<T>)
+            {
+                serialize(os, net_uint64(std::tuple_size_v<T>));
+                detail::expand_serialize(
+                    os, t, 
+                    std::make_index_sequence<std::tuple_size_v<T>>{});
+            }
+        }
+    }
 }
 
 } //namespace todolist::serialization
